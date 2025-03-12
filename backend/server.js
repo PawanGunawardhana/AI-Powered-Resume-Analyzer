962ce7d68d536119f69cd4feefeeed1294497c73
Pawan Gunawardhana <adpigunawardhana2@gmail.com>
2025-03-12 17:45:28 +0530

    Define backend server.js

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const resumeRoutes = require("./routes/resumeRoutes");
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use("/api/resume", resumeRoutes);
+
+app.listen(5000, () => console.log("Server running on port 5000"));
